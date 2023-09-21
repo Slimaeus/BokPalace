13bdd3a31408d82b7eb43dff785415f204d8d6fb
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-21 23:46:30 +0700

    Add Assembly Reference

---
+++ BokPalace.Application/AssemblyReference.cs
@@ -0,0 +1,13 @@
+﻿using BokPalace.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyModel;
+using System.Reflection;
+
+namespace BokPalace.Application;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+    public static readonly string? AssemblyName = typeof(ApplicationDbContext).Assembly.FullName;
+    public static readonly Assembly CallingAssembly = Assembly.GetCallingAssembly();
+    public static readonly DependencyContext? DependencyContext = DependencyContext.Default;
+}
