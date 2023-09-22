d1072d5b2839b34e606561ed5643ffd010696705
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-22 23:03:02 +0700

    Add Create Room method and request

---
+++ BokPalace.Web/Data/CreateRoomRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace BokPalace.Web.Data;
+
+public sealed record CreateRoomRequest(string Name, string? Description);
--- BokPalace.Web/Data/RoomService.cs
+++ BokPalace.Web/Data/RoomService.cs
@@ -5,4 +5,6 @@ public class RoomService : BaseApiService
     public RoomService(IHttpClientFactory httpClientFactory) : base(httpClientFactory) { }
     public Task<RoomDto[]?> GetRoomsAsync()
         => GetAsync<RoomDto[]?>("api/rooms");
+    public Task<RoomDto?> CreateRoomAsync(CreateRoomRequest request)
+        => PostAsync<RoomDto?>("api/rooms", request);
 }
