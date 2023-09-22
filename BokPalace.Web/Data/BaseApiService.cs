e285ab11613148cf7e2577935429619c9be40e50
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-22 16:25:10 +0700

    Add Base Api Service

---
+++ BokPalace.Web/Data/BaseApiService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace BokPalace.Web.Data;
+
+public abstract class BaseApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public BaseApiService(IHttpClientFactory httpClientFactory)
+        => _httpClient = httpClientFactory.CreateClient("BokPalace");
+    public async Task<TResult?> GetAsync<TResult>(string route)
+        => await DeserilizeContent<TResult>(await _httpClient.GetAsync(route));
+    public async Task<TResult?> PostAsync<TResult>(string route, object body)
+    {
+        var jsonContent = new StringContent(
+            JsonSerializer.Serialize(body),
+            Encoding.UTF8,
+            "application/json");
+        return await DeserilizeContent<TResult>(await _httpClient.PostAsync(route, jsonContent));
+    }
+
+    private static async Task<TResult?> DeserilizeContent<TResult>(HttpResponseMessage response)
+    {
+        try
+        {
+            var serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            return await JsonSerializer.DeserializeAsync<TResult>(await response.Content.ReadAsStreamAsync(), serializerOptions);
+        }
+        catch (Exception)
+        {
+            return default;
+        }
+    }
+}
--- BokPalace.Web/Data/RoomService.cs
+++ BokPalace.Web/Data/RoomService.cs
@@ -1,28 +1,8 @@
-﻿using System.Text.Json;
+﻿namespace BokPalace.Web.Data;
 
-namespace BokPalace.Web.Data;
-
-public class RoomService
+public class RoomService : BaseApiService
 {
-    private readonly HttpClient _httpClient;
-
-    public RoomService(IHttpClientFactory httpClientFactory)
-        => _httpClient = httpClientFactory.CreateClient("BokPalace");
-    public async Task<RoomDto[]?> GetRoomsAsync()
-    {
-        var result = await _httpClient.GetAsync("api/rooms");
-        var serializerOptions = new JsonSerializerOptions
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-        };
-        try
-        {
-            var rooms = await JsonSerializer.DeserializeAsync<RoomDto[]>(await result.Content.ReadAsStreamAsync(), serializerOptions);
-            return rooms;
-        }
-        catch (Exception)
-        {
-            return null;
-        }
-    }
+    public RoomService(IHttpClientFactory httpClientFactory) : base(httpClientFactory) { }
+    public Task<RoomDto[]?> GetRoomsAsync()
+        => GetAsync<RoomDto[]?>("api/rooms");
 }
